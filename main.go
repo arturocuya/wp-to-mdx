6a100e2b6b076384378fe551eaccb8704369275f
Arturo Cuya <arturocuya099@gmail.com>
2025-04-28 23:57:23 -0500

    added tags

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+
 package main
 
 import (
@@ -5,6 +6,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	html2md "github.com/JohannesKaufmann/html-to-markdown"
@@ -14,11 +16,16 @@ import (
 	"github.com/joho/godotenv"
 )
 
-// Post represents a WordPress post with title, publish date and HTML content.
+// Post represents a WordPress post with title, publish date, update date, HTML content, and related taxonomies.
 type Post struct {
-	Title         string `db:"title"`
-	PublishedDate string `db:"published_date"`
-	Content       string `db:"content"`
+	ID            int       `db:"ID"`
+	Title         string    `db:"title"`
+	PublishedDate string    `db:"published_date"`
+	UpdatedDate   string    `db:"updated_date"`
+	Content       string    `db:"content"`
+	Tags          []string  // Will be populated separately
+	Categories    []string  // Will be populated separately
+	IsFeatured    bool      // Default is false
 }
 
 func main() {
@@ -56,11 +63,13 @@ func main() {
 	}
 	defer db.Close()
 
-	// Query to fetch title, publish date and HTML content
+	// Query to fetch posts with title, publish date, update date and HTML content
 	query := `
         SELECT
+          ID,
           post_title   AS title,
           post_date    AS published_date,
+          post_modified AS updated_date,
           post_content AS content
         FROM wp_posts
         WHERE
@@ -75,6 +84,43 @@ func main() {
 	// Execute query and map results into the slice
 	if err := db.Select(&posts, query); err != nil {
 		log.Fatalf("Query execution error: %v", err)
+	}
+
+	// For each post, fetch its tags and categories
+	for i := range posts {
+		// Get tags for the post
+		var tags []string
+		tagQuery := `
+			SELECT t.name
+			FROM wp_terms t
+			INNER JOIN wp_term_taxonomy tt ON t.term_id = tt.term_id
+			INNER JOIN wp_term_relationships tr ON tt.term_taxonomy_id = tr.term_taxonomy_id
+			WHERE tr.object_id = ?
+			AND tt.taxonomy = 'post_tag';
+		`
+		if err := db.Select(&tags, tagQuery, posts[i].ID); err != nil {
+			log.Printf("Error fetching tags for post %d: %v", posts[i].ID, err)
+		}
+		posts[i].Tags = tags
+
+		// Get categories for the post
+		var categories []string
+		categoryQuery := `
+			SELECT t.name
+			FROM wp_terms t
+			INNER JOIN wp_term_taxonomy tt ON t.term_id = tt.term_id
+			INNER JOIN wp_term_relationships tr ON tt.term_taxonomy_id = tr.term_taxonomy_id
+			WHERE tr.object_id = ?
+			AND tt.taxonomy = 'category';
+		`
+		if err := db.Select(&categories, categoryQuery, posts[i].ID); err != nil {
+			log.Printf("Error fetching categories for post %d: %v", posts[i].ID, err)
+		}
+		posts[i].Categories = categories
+
+		// Merge categories into tags for the frontmatter schema
+		// This is because the schema only has a tags field, not categories
+		posts[i].Tags = append(posts[i].Tags, categories...)
 	}
 
 	// Create a new converter with default options
@@ -119,17 +165,13 @@ func main() {
 		},
 	)
 
-	// Print a snippet of each post
+	// Process each post
 	for _, post := range posts {
-		if err := os.WriteFile(fmt.Sprintf("./output-html/%s.html", post.Title), []byte(post.Content), 0644); err != nil {
-			// log.Fatalf("WriteFile error: %v", err)
-			continue
-		}
-
 		// Convert the HTML string to Markdown
 		markdown, err := converter.ConvertString(post.Content)
 		if err != nil {
-			log.Fatalf("Conversion error: %v", err)
+			log.Printf("Conversion error for post %s: %v", post.Title, err)
+			continue
 		}
 
 		post.Content = markdown
@@ -137,34 +179,100 @@ func main() {
 		contentLen := min(len(post.Content), 20)
 
 		fmt.Printf(
-			"Title: %s\nDate: %s\nContent snippet: %.60s...\n\n",
+			"Title: %s\nDate: %s\nTags: %s\nContent snippet: %.60s...\n\n",
 			post.Title,
 			post.PublishedDate,
+			strings.Join(post.Tags, ", "),
 			post.Content[:contentLen],
 		)
 
-		// Parse the date from the database format
-		publishDate, err := time.Parse(time.RFC3339, post.PublishedDate)
+		// Parse the publish date from the database format
+		// Try different date formats since WordPress can store dates in different formats
+		publishDate, err := parseWordPressDate(post.PublishedDate)
 		if err != nil {
-			log.Printf("Warning: Could not parse date '%s': %v", post.PublishedDate, err)
+			log.Printf("Warning: Could not parse publish date '%s': %v", post.PublishedDate, err)
 			publishDate = time.Now() // fallback to current time
 		}
 
-		// Create markdown content with frontmatter
-		markdownWithFrontmatter := fmt.Sprintf("---\ntitle: %s\nexcerpt: \"\"\npublishDate: %s\nisFeatured: false\ntags: []\nseo: {}\n---\n\n%s",
+		// Parse the update date from the database format
+		var updatedDateFrontmatter string
+		updatedDate, err := parseWordPressDate(post.UpdatedDate)
+		if err != nil {
+			log.Printf("Warning: Could not parse update date '%s': %v", post.UpdatedDate, err)
+			// If we can't parse the updated date, we'll omit it from the frontmatter
+		} else {
+			updatedDateFrontmatter = fmt.Sprintf("updatedDate: %s\n", strconv.Quote(updatedDate.Format("2006-01-02")))
+		}
+
+		// Format tags as a JSON array for the frontmatter
+		tagsJSON := "[]"
+		if len(post.Tags) > 0 {
+			quotedTags := make([]string, len(post.Tags))
+			for i, tag := range post.Tags {
+				quotedTags[i] = strconv.Quote(tag)
+			}
+			tagsJSON = fmt.Sprintf("[%s]", strings.Join(quotedTags, ", "))
+		}
+
+		// Create markdown content with frontmatter according to the schema
+		markdownWithFrontmatter := fmt.Sprintf("---\ntitle: %s\nexcerpt: \"\"\npublishDate: %s\n%sisFeatured: false\ntags: %s\nseo: {}\n---\n\n%s",
 			strconv.Quote(post.Title),
 			strconv.Quote(publishDate.Format("2006-01-02")),
+			updatedDateFrontmatter,
+			tagsJSON,
 			post.Content,
 		)
 
-		if err := os.WriteFile(fmt.Sprintf("%s/%s.md", outputDir, post.Title), []byte(markdownWithFrontmatter), 0644); err != nil {
-			// log.Fatalf("WriteFile error: %v", err)
+		// Sanitize filename to avoid issues with filesystem
+		safeTitle := sanitizeFilename(post.Title)
+
+		if err := os.WriteFile(fmt.Sprintf("%s/%s.md", outputDir, safeTitle), []byte(markdownWithFrontmatter), 0644); err != nil {
+			log.Printf("WriteFile error for %s: %v", safeTitle, err)
 			continue
 		}
 	}
 
-	fmt.Println("images to download")
+	fmt.Println("Images to download:")
 	for i, src := range imageURLs {
 		fmt.Println(i, src)
 	}
+}
+
+// parseWordPressDate attempts to parse a WordPress date string using multiple formats
+func parseWordPressDate(dateStr string) (time.Time, error) {
+	// List of possible date formats in WordPress
+	dateFormats := []string{
+		"2006-01-02 15:04:05",         // MySQL datetime format
+		time.RFC3339,                   // "2006-01-02T15:04:05Z07:00"
+		"2006-01-02T15:04:05-07:00",    // WordPress often uses this format
+		"2006-01-02T15:04:05.000-07:00", // With milliseconds
+		"2006-01-02T15:04:05.000Z",     // UTC with milliseconds
+	}
+
+	// Try each format until one works
+	for _, format := range dateFormats {
+		if date, err := time.Parse(format, dateStr); err == nil {
+			return date, nil
+		}
+	}
+
+	// None of the formats worked
+	return time.Time{}, fmt.Errorf("could not parse date using any known WordPress formats: %s", dateStr)
+}
+
+// sanitizeFilename removes characters that might cause problems in filenames
+func sanitizeFilename(filename string) string {
+	// Replace problematic characters with underscores
+	replacer := strings.NewReplacer(
+		"/", "_",
+		"\\", "_",
+		":", "_",
+		"*", "_",
+		"?", "_",
+		"\"", "_",
+		"<", "_",
+		">", "_",
+		"|", "_",
+	)
+	return replacer.Replace(filename)
 }
